fa9fa656715cfa223df866b86cce670cca7a34b0
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-31 18:12:21 +0100

    Finished main exercise

--- script.js
+++ script.js
@@ -4,24 +4,20 @@ $(document).ready(function() {
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
     success: function(data, state) {
-      console.log(data);
+      var arrayAlbum = data.response;
+      for (var i = 0; i < arrayAlbum.length; i++) {
+        var album = arrayAlbum[i];
+        // handlebars
+        var source = $('#template').html();
+        var template = Handlebars.compile(source);
+        var context = album;
+        var html = template(context);
+        $('.cds-container.container').append(html);
+      }
     },
     error: function(request, state, error) {
       console.log(error);
     }
   });
-
-
-  var source = $('#template').html();
-  var template = Handlebars.compile(source);
-
-  var context = {
-    'title': 'hello world',
-    'paragraph': 'lorem lorem is lorem'
-  };
-
-  var html = template(context);
-
-  $('body').append(html);
 
 });
